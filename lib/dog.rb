3d6e8620a2954790773d4c4396d3998047148188
Thomas John Helin <thomashelin@yahoo.com>
2020-04-23 19:10:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+require 'pry'
+class Dog
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+#binding.pry
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+fido = Dog.new
+fido.name
